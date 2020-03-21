d06a6fe52dacc0b1a1b413ed5dd946c2eedd3798
Igor Frolkin <frolkin_igor@mail.ru>
2020-03-21 20:07:34 +0300

    ds lib les 2

--- Databases/lesson_7/Script_1.sql
+++
@@ -1,5 +0,0 @@
-/*
- * Составьте список пользователей users, которые осуществили хотя бы один заказ orders в интернет магазине.
- */
-
-SELECT name FROM users WHERE id in (SELECT user_id FROM orders);
--- Databases/lesson_7/Script_2.sql
+++
@@ -1,7 +0,0 @@
-/*
- * Выведите список товаров products и разделов catalogs, который соответствует товару.
- */
-
-SELECT products.name, catalogs.name
-FROM products, catalogs
-WHERE products.catalog_id = catalogs.id
